55075f6e8d1d7ae5f91e3d3f4c5a699e228520d7
Kazuo Murakami <wired2wireless4me@gmail.com>
2024-06-08 12:24:59 +0900

    commit

--- app/root.tsx
+++ app/root.tsx
@@ -1,12 +1,51 @@
+import { LinksFunction, LoaderFunctionArgs, json, redirect } from "@remix-run/node";
 import {
   Form,
   Links,
-  Meta,
+  Meta, NavLink, Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
+  useNavigation,
+  useSubmit
 } from "@remix-run/react";
+import { useEffect } from "react"
+
+import appStylesHref from "./app.css?url"
+import { createEmptyContact, getContacts } from "./data"
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: appStylesHref },
+];
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const url = new URL(request.url)
+  const q = url.searchParams.get("q")
+  const contacts = await getContacts(q)
+  return json({ contacts, q })
+}
+
+export const action = async () => {
+  const contact = await createEmptyContact()
+  return redirect(`/contacts/${contact.id}/edit`)
+}
 
 export default function App() {
+  const { contacts, q } = useLoaderData<typeof loader>()
+  const navigation = useNavigation()
+  const submit = useSubmit()
+  const searching =
+      navigation.location &&
+      new URLSearchParams(navigation.location.search).has("q")
+
+  // NOTE: いらないかも
+  // useEffect(() => {
+  //   const searchField = document.getElementById("q")
+  //   if (searchField instanceof HTMLInputElement) {
+  //     searchField.value = q || ""
+  //   }
+  // }, [q])
+
   return (
     <html lang="en">
       <head>
@@ -16,37 +55,78 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <div id="sidebar">
-          <h1>Remix Contacts</h1>
-          <div>
-            <Form id="search-form" role="search">
-              <input
+      <div id="sidebar">
+        <h1>Remix Contacts</h1>
+        <div>
+          <Form id="search-form" role="search"
+            onChange={(event) => {
+              // ???
+              const isFirstSearch = q === null
+              submit(event.currentTarget, {
+                replace: !isFirstSearch,
+              });
+            }}
+          >
+            <input
                 id="q"
                 aria-label="Search contacts"
+                className={searching ? "loading" : ""}
+                defaultValue={q || ""}
                 placeholder="Search"
                 type="search"
                 name="q"
-              />
-              <div id="search-spinner" aria-hidden hidden={true} />
-            </Form>
-            <Form method="post">
-              <button type="submit">New</button>
-            </Form>
-          </div>
-          <nav>
-            <ul>
-              <li>
-                <a href={`/contacts/1`}>Your Name</a>
-              </li>
-              <li>
-                <a href={`/contacts/2`}>Your Friend</a>
-              </li>
-            </ul>
-          </nav>
+            />
+            <div id="search-spinner" aria-hidden hidden={!searching}/>
+          </Form>
+          <Form method="post">
+            <button type="submit">New</button>
+          </Form>
         </div>
+        <nav>
+          {contacts.length ? (
+              <ul>
+                {contacts.map((contact) => (
+                    <li key={contact.id}>
+                      <NavLink
+                          className={({ isActive, isPending}) =>
+                              isActive
+                                ? "active"
+                                : isPending
+                                ? "pending"
+                                : ""
+                          }
+                          to={`contacts/${contact.id}`}>
+                        {contact.first || contact.last ? (
+                            <>
+                              {contact.first} {contact.last}
+                            </>
+                        ) : (
+                            <i>No Name</i>
+                        )}{" "}
+                        {contact.favorite ? (
+                            <span>★</span>
+                        ) : null}
+                      </NavLink>
+                    </li>
+                ))}
+              </ul>
+          ) : (
+              <p>
+                <i>No contacts</i>
+              </p>
+          )}
+        </nav>
+      </div>
+      <div
+          className={
+            navigation.state === "loading" ? "loading" : ""
+          }
+          id="detail">
+        <Outlet/>
+      </div>
 
-        <ScrollRestoration />
-        <Scripts />
+      <ScrollRestoration/>
+      <Scripts/>
       </body>
     </html>
   );
